18004a5a2009bb729ae43cd3bbe388e578c14e1b
Thomas Manu <thomas@thomass-mbp.att.net>
2017-01-05 16:44:49 -0500

    Initial Commit

---
+++ Clangle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Clangle
+//
+//  Created by Thomas Manu on 1/5/17.
+//  Copyright © 2017 Thomas Manu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Clangle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clangle
+//
+//  Created by Thomas Manu on 1/5/17.
+//  Copyright © 2017 Thomas Manu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
