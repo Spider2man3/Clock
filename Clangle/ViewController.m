221d6369e5900118c7bd6da25a1eca60245bdde1
Thomas Manu <thomas@thomass-mbp.att.net>
2017-01-05 17:14:57 -0500

    Trials

--- Clangle/ViewController.m
+++ Clangle/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     printf("Let's hope this works");
+    NSString *x = @"This works!";
 }
 
 
