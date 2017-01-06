de005cb6743815b02e9c723152f056bdc5039fcf
Thomas M <thomasm@southern.edu>
2017-01-05 20:52:32 -0500

    Add a digital clock and clock angle

--- Clangle/ViewController.m
+++ Clangle/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *angleLabel;
 
 @end
 
@@ -17,8 +19,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    printf("Let's hope this works");
-    NSString *x = @"This works!";
+    
+    // Grab system date once view is loaded.
+    NSDate *today = [NSDate date];
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"hh:mm a"];
+    NSString *dateString = [dateFormat stringFromDate:today];
+    [self.dateLabel setText:dateString];
+    
+    // Find and display shortest angle between hour and minute hand.
+    NSCalendar *cal = [NSCalendar currentCalendar];
+    NSDateComponents *components = [cal components:(NSCalendarUnitHour | NSCalendarUnitMinute) fromDate:today];
+    NSInteger hour = [components hour];
+    NSInteger minute = [components minute];
+    if (hour >= 12) {hour -= 12;}
+    NSInteger angleBetweenHourMinute = ABS((hour * 30) - (minute * 6));
+    [self.angleLabel setText:[NSString stringWithFormat:@"%ldº", (long)angleBetweenHourMinute]];
 }
 
 
